caed5584ff55f32052fe77b460047988524ee29c
Alexander Burger <abu@software-lab.de>
2011-10-11 17:16:42 +0200

    '*Prompt' global variable

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/* 09sep11abu
+/* 11oct11abu
  * (c) Software Lab. Alexander Burger
  */
 
@@ -28,7 +28,7 @@ any Alarm, Sigio, Line, Zero, One;
 any Intern[IHASH], Transient[IHASH], Extern[EHASH];
 any ApplyArgs, ApplyBody, DbVal, DbTail;
 any Nil, DB, Meth, Quote, T;
-any Solo, PPid, Pid, At, At2, At3, This, Dbg, Zap, Ext, Scl, Class;
+any Solo, PPid, Pid, At, At2, At3, This, Prompt, Dbg, Zap, Ext, Scl, Class;
 any Run, Hup, Sig1, Sig2, Up, Err, Msg, Uni, Led, Tsm, Adr, Fork, Bye;
 bool Break;
 sig_atomic_t Signal[NSIG];
